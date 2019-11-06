412a7abe093573bc7736b3b8b7adcdbf223695cb
Neil Mehra <45741682+AsianKoala@users.noreply.github.com>
2019-11-06 17:52:51 -0500

    need to fix imports still

--- SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lastMinutescuffedPP/Movement/RobotMovement.java
+++ SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lastMinutescuffedPP/Movement/RobotMovement.java
@@ -1,8 +1,8 @@
 package org.firstinspires.ftc.teamcode.lastMinutescuffedPP.Movement;
 
 import org.firstinspires.ftc.teamcode.ppProject.company.ComputerDebugging;
-import org.firstinspires.ftc.teamcode.ppProject.company.FloatPoint;
-import org.firstinspires.ftc.teamcode.ppProject.company.Range;
+import org.firstinspires.ftc.teamcode.lastMinutescuffedPP.HelperClasses.FloatPoint;
+import org.firstinspires.ftc.teamcode.ppProject.company.Range; // TODO: FI ALL THESE DAMN IMPORTS REEE
 import org.firstinspires.ftc.teamcode.ppProject.core.Point;
 import org.firstinspires.ftc.teamcode.ppProject.treamcode.CurvePoint;
 import org.firstinspires.ftc.teamcode.ppProject.treamcode.MathFunctions;
@@ -97,10 +97,51 @@ public class RobotMovement {
         }
 
 
-
         movement_turn = turnPower;
         movement_x = movementXPower;
         movement_y = movementYPower;
        
     }
+
+
+    public static CurvePoint getFollowPointPath(ArrayList<CurvePoint> pathPoints, Point robotLocation, double followRadius) {
+        CurvePoint followMe = new CurvePoint(pathPoints.get(0));
+
+        for(int i = 0; i < pathPoints.size() - 1; i++) {
+            CurvePoint startLine = pathPoints.get(i);
+            CurvePoint endLine = pathPoints.get(i + 1);
+
+            ArrayList<Point> intersections = lineCircleIntersection(robotLocation, followRadius, startLine.toPoint(), endLine.toPoint());
+
+
+
+            double closestAngle = 100000;
+
+            for (Point thisIntersection : intersections) {
+                double angle = Math.atan2(thisIntersection.y - worldYPosition, thisIntersection.x - worldXPosition);
+                double deltaAngle = Math.abs(MathFunctions.AngleWrap(angle - worldAngle_rad));
+
+                if(deltaAngle < closestAngle) {
+                    closestAngle = deltaAngle;
+                    followMe.setPoint(thisIntersection);
+                }
+            }
+        }
+        return followMe;
+    }
+
+    public static void followCurve (ArrayList<CurvePoint> allPoints, double followAngle) {
+        for(int i = 0; i < allPoints.size() - 1; i++) {
+            ComputerDebugging.sendLine(new FloatPoint(allPoints.get(i).x, allPoints.get(i).y),
+                    new FloatPoint(allPoints.get(i+1).x, allPoints.get(i+1).y));
+        }
+
+        CurvePoint followMe = getFollowPointPath(allPoints, new Point(worldXPosition, worldYPosition) ,
+                allPoints.get(0).followDistance);
+
+        ComputerDebugging.sendKeyPoint(new FloatPoint(followMe.x, followMe.y));
+
+        goToPosition(followMe.x, followMe.y, followMe.moveSpeed, followAngle, followMe.turnSpeed); // important thing here
+    }
+
 }
--- SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lastMinutescuffedPP/teemode/DebuggingTest.java
+++ SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lastMinutescuffedPP/teemode/DebuggingTest.java
@@ -1,22 +1,22 @@
-package org.firstinspires.ftc.teamcode.ppProject.treamcode;
+package org.firstinspires.ftc.teamcode.lastMinutescuffedPP.teemode;
 
+import org.firstinspires.ftc.teamcode.lastMinutescuffedPP.HelperClasses.CurvePoint;
+import static org.firstinspires.ftc.teamcode.lastMinutescuffedPP.Movement.RobotMovement.followCurve;
 import java.util.ArrayList;
-import static org.firstinspires.ftc.teamcode.ppProject.treamcode.RobotMovement.followCurve;
 
-
-public class MyOpMode extends OpMode {
-
+public class DebuggingTest extends DebuggingOpMode {
     @Override
     public void init() {
+
     }
 
     @Override
     public void loop() {
-       ArrayList<CurvePoint> allPoints = new ArrayList<>();
+        ArrayList<CurvePoint> allPoints = new ArrayList<>();
         allPoints.add(new CurvePoint(180,180,1.0,1,20,Math.toRadians(50),1.0));
         allPoints.add(new CurvePoint(220,180,1,1,20, Math.toRadians(50),1.0));
         allPoints.add(new CurvePoint(280,100,1,1,20, Math.toRadians(50),1.0));
         allPoints.add(new CurvePoint(280,0,1,1,20, Math.toRadians(50),1.0));
-        followCurve(allPoints, Math.toRadians(90));
+        followCurve(allPoints, Math.PI/2);
     }
 }
