8e7582a032050749bf6b105dd21d98731d2ae789
neilm <neilmehra@outlook.com>
2019-11-10 01:43:08 -0500

    deleted some unnecessary complications and added vuforia to auto opmode

--- SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/teamcode/drive/opmode/autoPaths/autoPathRed.java
+++
@@ -1,7 +0,0 @@
-package org.firstinspires.ftc.teamcode.roadrunner.teamcode.drive.opmode.autoPaths;
-
-import com.acmerobotics.roadrunner.trajectory.TrajectoryBuilder;
-
-public class autoPathRed {
-    Trajec
-}
