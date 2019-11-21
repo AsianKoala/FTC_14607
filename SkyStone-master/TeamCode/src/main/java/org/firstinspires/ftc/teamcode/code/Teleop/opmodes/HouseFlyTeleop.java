418941c4892d8fe56bafa558913d468961e0b965
cwu159 <cwu159@gmail.com>
2019-11-21 15:41:34 -0500

    2 axis arm controls and rad->deg fix

--- SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/code/Teleop/opmodes/HouseFlyTeleop.java
+++ SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/code/Teleop/opmodes/HouseFlyTeleop.java
@@ -49,6 +49,30 @@ public class HouseFlyTeleop extends OpMode {
         leftSlide.setPower(-gamepad2.right_stick_y);
         rightSlide.setPower(-gamepad2.right_stick_y);
 
+        // flipper arm control
+        if(gamepad2.right_trigger > 0.5) {
+            robot.flip();
+        }
+        if(gamepad2.left_trigger > 0.5) {
+            robot.flipReady();
+        }
+
+        // rotater arm control
+        if(gamepad2.right_bumper) {
+            robot.rotaterOut();
+        }
+        if(gamepad2.left_bumper) {
+            robot.rotaterReady();
+        }
+
+        // gripper arm control
+        if(gamepad2.a) {
+            robot.grip();
+        }
+        if(gamepad2.y) {
+            robot.gripReady();
+        }
+
 
 
         // drivetrain control
@@ -79,16 +103,17 @@ public class HouseFlyTeleop extends OpMode {
                 heading = robot.getRawExternalHeading();
 
                 double magnitude = Math.sqrt(Math.pow(gamepad1.left_stick_y, 2) + Math.pow(gamepad1.left_stick_x, 2));
-                double driveHeading = Math.atan(gamepad1.left_stick_y/gamepad1.left_stick_x);
-                if(gamepad1.left_stick_y < 0 && gamepad1.left_stick_x < 0) {
-                    driveHeading += 180.0;
-                }
-                if(gamepad1.left_stick_y < 0 && gamepad1.left_stick_x > 0) {
-                    // then arctan returns correct value
-                }
-                if(gamepad1.left_stick_y > 0 && gamepad1.left_stick_x < 0) {
-                    driveHeading += 180.0;
-                }
+                double driveHeading = Math.atan2(gamepad1.left_stick_y/gamepad1.left_stick_x);
+                driveHeading *= 180/Math.PI;
+//                if(gamepad1.left_stick_y < 0 && gamepad1.left_stick_x < 0) {
+//                    driveHeading += 180.0;
+//                }
+//                if(gamepad1.left_stick_y < 0 && gamepad1.left_stick_x > 0) {
+//                    // then arctan returns correct value
+//                }
+//                if(gamepad1.left_stick_y > 0 && gamepad1.left_stick_x < 0) {
+//                    driveHeading += 180.0;
+//                }
                 double theta = driveHeading-heading;
                 double robotXcomponent = Math.cos(theta) * magnitude;
                 double robotYcomponent = Math.sin(theta) * magnitude;
