0371df06c98ea8a9b0a255e121a2ded9e74fadf7
neilm <neilmehra@outlook.com>
2019-11-26 21:01:24 -0500

    finished intake hardware class

--- SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/code/Hardware/Intake.java
+++ SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/code/Hardware/Intake.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.code.Hardware;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import org.openftc.revextensions2.ExpansionHubMotor;
 
 import java.util.ArrayList;
@@ -10,6 +11,20 @@ public class Intake {
     public ExpansionHubMotor leftIntake;
     public ExpansionHubMotor rightIntake;
     public ArrayList<ExpansionHubMotor> allMotors = new ArrayList<>();
+
+    private final double maxSpeed = 0.75;
+    private double leftIntakeSpeed = 0;
+    private double rightIntakeSpeed = 0;
+
+    INTAKE_POWERS lastIntakePower = INTAKE_POWERS.OFF;
+    INTAKE_POWERS intakePowers = INTAKE_POWERS.OFF;
+
+    enum INTAKE_POWERS {
+        ON,
+        OFF,
+        REVERSE,
+        CUSTOM
+    }
 
     public Intake(ExpansionHubMotor leftIntake, ExpansionHubMotor rightIntake) {
         this.leftIntake = leftIntake;
@@ -21,11 +36,61 @@ public class Intake {
             expansionHubMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
             expansionHubMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         }
+
+        rightIntake.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        intakePowers = INTAKE_POWERS.OFF;
     }
 
 
 
-    public void setIntakePowers(Pose2d pose) {
 
+    private void setIntakePowers(double leftPower, double rightPower) {
+        leftIntakeSpeed = leftPower;
+        rightIntakeSpeed = rightPower;
     }
+
+
+    public void turnOnIntake() { intakePowers = INTAKE_POWERS.ON; }
+    public void turnOffIntake() { intakePowers = INTAKE_POWERS.OFF; }
+    public void reverseIntake() { intakePowers = INTAKE_POWERS.REVERSE; }
+    public void manualControl(double customLeftIntakeSpeed, double customRightIntakeSpeed) {
+        intakePowers = INTAKE_POWERS.CUSTOM;
+        leftIntakeSpeed = customLeftIntakeSpeed;
+        rightIntakeSpeed = customRightIntakeSpeed;
+    }
+
+
+    private void HandlePower() {
+        if(intakePowers == INTAKE_POWERS.ON) {
+            setIntakePowers(maxSpeed, maxSpeed);
+        }
+
+        if(intakePowers == INTAKE_POWERS.REVERSE) {
+            setIntakePowers(-maxSpeed, -maxSpeed);
+        }
+
+        if(intakePowers == INTAKE_POWERS.OFF) {
+            setIntakePowers(0,0);
+        }
+
+        if(intakePowers == INTAKE_POWERS.CUSTOM) {
+
+        }
+    }
+
+
+
+    public void update() {
+        // avoid spamming to make loop faster
+        if(lastIntakePower != intakePowers) {
+            HandlePower();
+            leftIntake.setPower(leftIntakeSpeed);
+            rightIntake.setPower(rightIntakeSpeed);
+
+            lastIntakePower = intakePowers;
+        }
+    }
+
+
 }
--- SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/code/Hardware/Slide.java
+++ SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/code/Hardware/Slide.java
@@ -1,4 +1,40 @@
 package org.firstinspires.ftc.teamcode.code.Hardware;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.PIDCoefficients;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+import org.openftc.revextensions2.ExpansionHubMotor;
+
+import java.util.ArrayList;
+
 public class Slide {
+    public ExpansionHubMotor leftSlide;
+    public ExpansionHubMotor rightSlide;
+    public ArrayList<ExpansionHubMotor> allMotors = new ArrayList<>();
+
+    public Slide(ExpansionHubMotor leftSlide, ExpansionHubMotor rightSlide) {
+        this.leftSlide = leftSlide;
+        this.rightSlide = rightSlide;
+        allMotors.add(leftSlide);
+        allMotors.add(rightSlide);
+
+        for(ExpansionHubMotor expansionHubMotor : allMotors) {
+            expansionHubMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            expansionHubMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        }
+
+        rightSlide.setDirection(DcMotor.Direction.REVERSE);
+    }
+
+    public void setPIDCoeffs(PIDCoefficients coeffs) {
+        leftSlide.setPIDCoefficients(DcMotor.RunMode.RUN_TO_POSITION, coeffs);
+        rightSlide.setPIDCoefficients(DcMotor.RunMode.RUN_TO_POSITION, coeffs);
+    }
+
+    public PIDCoefficients getPIDCoeffs() {
+        return leftSlide.getPIDCoefficients(DcMotor.RunMode.RUN_TO_POSITION);
+    }
+
+
 }
