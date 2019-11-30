e33771ee9a064858a4772cfac8bdb52c31977598
neilm <neilmehra@outlook.com>
2019-11-30 14:24:05 -0500

    initial commit from new local branch

---
+++ SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/code/Auto/ppProject/RobotUtilities/MovementVars.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode.code.Auto.ppProject.RobotUtilities;
+
+public class MovementVars {
+    public static double movement_x = 0;
+    public static double movement_y = 0;
+    public static double movement_turn = 0;
+}
---
+++ SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/code/Auto/ppProject/treamcode/OpMode.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode.code.Auto.ppProject.treamcode;
+
+public abstract class OpMode {
+
+    public abstract void init();
+    public abstract void loop();
+}
