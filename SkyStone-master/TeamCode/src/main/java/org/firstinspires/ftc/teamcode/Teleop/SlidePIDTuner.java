2e1473665654f1f57f670ae76c4b2c0bfc5d8a94
neilm <neilmehra@outlook.com>
2019-11-30 15:01:42 -0500

    moved slide pid tuner

--- SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/SlidePIDTuner.java
+++ SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/SlidePIDTuner.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.Teleop;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
