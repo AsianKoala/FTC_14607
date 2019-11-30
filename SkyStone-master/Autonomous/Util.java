34e7de38193244457bcb7c66268d592419394057
neilm <neilmehra@outlook.com>
2019-11-30 14:27:59 -0500

    removed other stuff

--- SkyStone-master/Autonomous/Util.java
+++
@@ -1,9 +0,0 @@
-package org.firstinspires.ftc.teamcode.treamcodde.Autonomous;
-
-import com.acmerobotics.roadrunner.geometry.Pose2d;
-
-public class Util {
-    public static Pose2d changeSide(Pose2d pose) {
-        return new Pose2d(pose.getX(), pose.getY(), pose.getHeading() * -1 );
-    }
-}
