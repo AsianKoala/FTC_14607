dc30dafe4726f0b9e53e69df98c517f7ff78649c
asiankoala <neilmehra@outlook.com>
2020-11-30 03:08:11 -0500

    added 2 lines to update code xd

--- Ultimate Goal-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/BaseOpMode.java
+++ Ultimate Goal-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/BaseOpMode.java
@@ -1,5 +1,8 @@
 package org.firstinspires.ftc.teamcode.opmodes;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
+
 import net.frogbots.ftcopmodetunercommon.opmode.TunableOpMode;
 
 import org.firstinspires.ftc.teamcode.hardware.DriveTrain;
@@ -15,6 +18,9 @@ public class BaseOpMode extends TunableOpMode {
 
     private OdometryPositioning odometryPositioning;
     private ExpansionHubMotor leftYAxisEncoder, rightYAxisEncoder, horizontalXEncoder;
+    private Rev2mDistanceSensor forwardDistanceSensor, leftDistanceSensor, rightDistanceSensor, backDistanceSensor;
+
+    private BNO055IMU imu;
 
     @Override
     public void init() {
@@ -27,10 +33,26 @@ public class BaseOpMode extends TunableOpMode {
         rightBack = hardwareMap.get(ExpansionHubMotor.class, "rightBack");
         driveTrain = new DriveTrain(leftFront, leftBack, rightFront, rightBack);
 
+
+        // odom
         leftYAxisEncoder = hardwareMap.get(ExpansionHubMotor.class, "leftXAxisEncoder");
         rightYAxisEncoder = hardwareMap.get(ExpansionHubMotor.class, "rightXAxisEncoder");
         horizontalXEncoder = hardwareMap.get(ExpansionHubMotor.class, "horizontalYAxisEncoder");
-        odometryPositioning = new OdometryPositioning(leftYAxisEncoder, rightYAxisEncoder, horizontalXEncoder);
+        forwardDistanceSensor = hardwareMap.get(Rev2mDistanceSensor.class, "fwdDistance");
+        rightDistanceSensor = hardwareMap.get(Rev2mDistanceSensor.class, "rightDistance");
+        backDistanceSensor = hardwareMap.get(Rev2mDistanceSensor.class, "backDistance");
+        leftDistanceSensor = hardwareMap.get(Rev2mDistanceSensor.class, "leftDistance");
+        odometryPositioning = new OdometryPositioning(leftYAxisEncoder, rightYAxisEncoder, horizontalXEncoder, forwardDistanceSensor, rightDistanceSensor, backDistanceSensor, leftDistanceSensor);
+
+        // imu
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.RADIANS;
+        imu.initialize(parameters);
+
+
+        // for debugging set init position at x=0, y=0, heading=0
+        odometryPositioning.setPosition(0,0,0);
     }
 
     @Override
@@ -45,6 +67,9 @@ public class BaseOpMode extends TunableOpMode {
 
     @Override
     public void loop() {
+
         driveTrain.update();
+
+        odometryPositioning.update();
     }
 }
--- Ultimate Goal-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TeleOp.java
+++ Ultimate Goal-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TeleOp.java
@@ -2,5 +2,23 @@ package org.firstinspires.ftc.teamcode.opmodes;
 
 public class TeleOp extends BaseOpMode {
 
+    @Override
+    public void init() {
+        super.init();
+    }
 
+    @Override
+    public void init_loop() {
+        super.init_loop();
+    }
+
+    @Override
+    public void start() {
+        super.start();
+    }
+
+    @Override
+    public void loop() {
+        super.loop();
+    }
 }
