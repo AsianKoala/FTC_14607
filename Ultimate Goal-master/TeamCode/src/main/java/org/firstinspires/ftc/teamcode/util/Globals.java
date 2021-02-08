ef14eaa8c56e9c082dfd83271f63a10313968c3d
asiankoala <neilmehra@outlook.com>
2021-02-08 11:14:39 -0500

    :flushed

--- Ultimate Goal-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Globals.java
+++
@@ -1,8 +0,0 @@
-package org.firstinspires.ftc.teamcode.util;
-
-public class Globals {
-    public static double movementX, movementY, movementTurn; // motor power calculations, changed within movement classes
-    public static boolean isDebugging; // turn this on for debugging mode
-    public static double globalX, globalY, globalAngle;
-    public static double oldX, oldY, oldAngle;
-}
