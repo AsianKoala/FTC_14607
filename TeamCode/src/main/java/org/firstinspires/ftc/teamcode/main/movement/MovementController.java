a2cf4b27aaa4427eb508e047d49babfa8be34320
Neil Mehra <neilmehra@outlook.com>
2021-02-23 21:46:00 -0500

    commented details for future additions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main/movement/MovementController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main/movement/MovementController.java
@@ -221,6 +221,9 @@ public class MovementController {
             }
         }
 
+        // USE ENUM MAP FOR CONDITION
+        // actually maybe just use it for functions too?? or maybe even implemented into statemachine, stagestartvars, stages, purepursuitpath, etc;
+        // https://www.geeksforgeeks.org/enummap-class-java-example/
 
 
         double absDeltaAngle = Math.abs(Util.angleWrap(currFollowAngle - currentPosition.heading));
