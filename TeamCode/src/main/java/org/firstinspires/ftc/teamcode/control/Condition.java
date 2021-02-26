9671897956a1a16a87cee43c3d295bcdc3b9ff1f
Neil Mehra <neilmehra@outlook.com>
2021-02-26 18:51:01 -0500

    properly deleted the the files

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/Condition.java
+++
@@ -1,8 +0,0 @@
-package org.firstinspires.ftc.teamcode.main.control;
-
-public class Condition {
-    // USE ENUM MAP FOR CONDITION
-    // actually maybe just use it for functions too?? or maybe even implemented into statemachine, stagestartvars, stages, purepursuitpath, etc;
-    // https://www.geeksforgeeks.org/enummap-class-java-example/
-
-}
