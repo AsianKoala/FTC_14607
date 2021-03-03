e5ba6f973f52a1e2ca6d043b03e2cb7be7617260
Neil Mehra <neilmehra@outlook.com>
2021-03-03 04:57:46 -0500

    yeah

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/README.md
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/README.md
@@ -1,3 +1,11 @@
 welcome to our amazing
 ## teamcode folder  
 maintained by yours truly, [Neil Mehra](https://github.com/AsianKoala)
+
+
+
+### todo
+1. fix vel calc
+2. optimize point/pose calc method returns
+3. fix followcurve
+4. test using debugger
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/controllers/PurePursuitController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/controllers/PurePursuitController.java
@@ -7,23 +7,22 @@ import org.firstinspires.ftc.teamcode.util.Pose;
 
 public class PurePursuitController {
     public static Pose max_speed = new Pose(1, 1, 1); // xd
+    public static Pose stop_speed = new Pose(0.1, 0.1, 0.1);
 
     public static void goToPosition(Robot robot, PathPoint target) {
-        Pose relDistances  = robot.currPose.distancePose(target);
+        Pose relVals = robot.currPose.distancePose(target);
 
-        double v = relDistances.abs().x + relDistances.abs().y;
-        Pose powerPose = new Pose(relDistances.x, relDistances.y, 69420); // heading doesnt matter for now so ya
+        double v = relVals.abs().x + relVals.abs().y;
+        Pose powerPose = new Pose(relVals.x, relVals.y, 69420); // heading doesnt matter for now so ya
         powerPose.divide(new Pose(v));
-        powerPose.multiply(relDistances.abs().divide(new Pose(12)));
+        powerPose.multiply(relVals.abs().divide(new Pose(12)));
 
         powerPose.heading = dH(robot, target) / Math.toRadians(45);
         robot.currPowers.set(powerPose.clipMax(max_speed));
 
         if(robot.currPose.distance(target) < 6 && target.stop) {
-            double exp = 1.0/6.0;
-            Pose speedSgns = robot.currPowers.sgns();
-            Pose absSpeeds = robot.currPowers.abs();
-            robot.currPowers = absSpeeds.pow(new Pose(exp)).multiply(speedSgns);
+            relVals = robot.currPose.distancePose(target);
+            robot.currPowers.set(relVals.multiply(stop_speed));
         }
 
     }
