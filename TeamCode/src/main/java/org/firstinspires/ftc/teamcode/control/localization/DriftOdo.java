22fe16b44907ed7bba0c122ed437ecaf74716db4
Neil <neilmehra@outlook.com>
2021-08-01 15:43:47 -0400

    fixed tracker coeffs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/localization/DriftOdo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/localization/DriftOdo.java
@@ -7,56 +7,51 @@ import org.firstinspires.ftc.teamcode.util.Pose;
 import org.openftc.revextensions2.RevBulkData;
 
 public class DriftOdo {
-    // turn 90
-    //
-    //
-    final Point tracker_coeffs = new Point(8672 / TICKS_PER_INCH, -7158 / TICKS_PER_INCH);
+    public static final double TICKS_PER_INCH = 1103.8839;
+    public static final Point tracker_coeffs = new Point(8272.5 / TICKS_PER_INCH, -8651 / TICKS_PER_INCH); //8672 -7158
 
-    private static final double TICKS_PER_INCH = 1103.8839;
-
-    private int prevVertical;
-    private int prevHorizontal;
-    private double prevHeading;
+    public static final int horiz_port = 0;
+    public static final int parallel_port = 2;
 
     public static double startHeading;
-
     private final Pose currentPosition;
+
+    public Point curr_wheels;
+    public Point delta_scaled;
+    public Point tracker_scaled;
+    public Point corrected_deltas;
+
+    public Point prev_wheels;
+    public double prevHeading;
 
     public DriftOdo(Pose start) {
         startHeading = start.h;
-        prevHorizontal = 0;
-        prevVertical = 0;
+        prev_wheels = new Point();
         prevHeading = startHeading;
 
         currentPosition = start;
     }
 
     public Pose update(Azusa azusa, double heading, RevBulkData data) {
-        int horiz_port = 0;
-        int curr_horiz = data.getMotorCurrentPosition(horiz_port);
-        int parallel_port = 2;
-        int curr_vert = data.getMotorCurrentPosition(parallel_port);
-        double deltaX = (curr_horiz - prevHorizontal) / TICKS_PER_INCH;
-        double deltaY = (curr_vert - prevVertical) / TICKS_PER_INCH;
+        curr_wheels = new Point(
+                data.getMotorCurrentPosition(horiz_port),
+                data.getMotorCurrentPosition(parallel_port));
+        delta_scaled = (curr_wheels.minus(prev_wheels)).scale(1 / TICKS_PER_INCH);
         double deltaAngle = MathUtil.angleWrap(heading - prevHeading);
 
-        Point tracker_scaled = tracker_coeffs.scale(1 / (Math.PI / 2));
-        double corrected_delta_x = deltaX - deltaAngle * tracker_scaled.x;
-        double corrected_delta_y = deltaY - deltaAngle * tracker_scaled.y;
+        tracker_scaled = tracker_coeffs.scale(1 / (Math.PI / 2));
+        corrected_deltas = delta_scaled.minus(tracker_scaled.scale(deltaAngle));
 
-        currentPosition.x += -(Math.cos(heading) * corrected_delta_y) + (Math.sin(heading) * corrected_delta_x);
-        currentPosition.y += -(Math.sin(heading) * corrected_delta_y) - (Math.cos(heading) * corrected_delta_x);
-        currentPosition.h = heading + startHeading;
+        currentPosition.x += -(Math.cos(heading) * corrected_deltas.y) + (Math.sin(heading) * corrected_deltas.x);
+        currentPosition.y += -(Math.sin(heading) * corrected_deltas.y) - (Math.cos(heading) * corrected_deltas.x);
+        currentPosition.h = MathUtil.angleWrap(heading + startHeading);
 
-        azusa.telemetry.addData("curr x", curr_horiz);
-        azusa.telemetry.addData("curr y", curr_vert);
-        azusa.telemetry.addData("delta x", corrected_delta_x);
-        azusa.telemetry.addData("delta y", corrected_delta_y);
+        azusa.telemetry.addData("curr", curr_wheels);
+        azusa.telemetry.addData("corrected", corrected_deltas);
         azusa.telemetry.addData("delta H", deltaAngle);
-        azusa.telemetry.addData("last horizontal", prevHorizontal);
-        azusa.telemetry.addData("last vertical", prevVertical);
-        prevHorizontal = curr_horiz;
-        prevVertical = curr_vert;
+        azusa.telemetry.addData("prev wheels", prev_wheels);
+
+        prev_wheels = curr_wheels;
         prevHeading = currentPosition.h;
         return currentPosition;
     }
