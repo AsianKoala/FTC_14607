933b0f13b3fb19b43ea41e54182984ed792f80b9
evlynlii <evlijai22@gmail.com>
2021-08-08 08:37:23 -0400

    aa

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/BasicTeleOp.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/BasicTeleOp.kt
@@ -1,4 +1,30 @@
 package org.firstinspires.ftc.teamcode.opmodes
 
-class BasicTeleOp {
+import com.qualcomm.robotcore.eventloop.opmode.OpMode
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import org.firstinspires.ftc.teamcode.hardware.DriveTrain
+import org.firstinspires.ftc.teamcode.util.Pose
+
+@TeleOp
+
+class BasicTeleOp : OpMode() {
+    override fun init() {
+        telemetry.update()
+    }
+
+    private lateinit var driveTrain: DriveTrain
+
+    override fun loop() {
+        //super.loop()
+        controlGamePad()
+    }
+
+    private fun controlGamePad() {
+        val x = -gamepad1.left_stick_x * 1.0
+        val y = gamepad1.left_stick_y * 1.0
+        val h = -gamepad1.right_stick_x * 1.0
+
+        driveTrain.powers = Pose(x, y, h)
+
+    }
 }
