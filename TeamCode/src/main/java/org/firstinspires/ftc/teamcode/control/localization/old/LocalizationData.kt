addc3b9e3ee1e606d023244e45ef484f081844a0
Neil Mehra <neilmehra@outlook.com>
2021-08-25 00:36:23 -0400

    refactor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/localization/old/LocalizationData.kt
+++
@@ -1,5 +0,0 @@
-package org.firstinspires.ftc.teamcode.control.localization.old
-
-import org.firstinspires.ftc.teamcode.util.math.Pose
-
-data class LocalizationData(val finalPose: Pose, val deltaXVec: Double, val deltaYVec: Double)
